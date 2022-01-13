8aeb12b6f471a42d478eeff4e22c8f87cd354a80
John Schottler <jcschottler1991@gmail.com>
2022-01-13 13:33:41 -0700

    ignore similar error

--- appinsights-slack-alerter-http-endpoint.js
+++ appinsights-slack-alerter-http-endpoint.js
@@ -91,7 +91,7 @@ function filterAppInsightsWeCareAbout(errors) {
     // Seems like they maybe happen in an external script since the assembly is "Unknown"--it's usually a url to a script file.
     const assemblyIsUnknown = error.assembly === 'Unknown'
     if (assemblyIsUnknown && error.type?.includes('TypeError: Load failed')) return false
-    if (assemblyIsUnknown && error.type?.includes('[object String]"Timeout (h)"')) return false
+    if (assemblyIsUnknown && error.type?.includes('[object String]"Timeout')) return false
       
     
     // chrome extension errors should usually not affect our app's code. And we can't really do anything til a particular person actually complains about something not working and then we can look up errors from them to find these and suggest they disable chrome extensions to see if it resolves the issue
